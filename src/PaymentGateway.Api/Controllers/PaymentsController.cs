16ad4d8f291977a78fc782f0316f82c831b1347e
Fares Eid <fareseid@gmail.com>
2026-03-13 14:21:35 +0100

    initial push

---
+++ src/PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using PaymentGateway.Api.Models.Responses;
+using PaymentGateway.Api.Services;
+
+namespace PaymentGateway.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PaymentsController : Controller
+{
+    private readonly PaymentsRepository _paymentsRepository;
+
+    public PaymentsController(PaymentsRepository paymentsRepository)
+    {
+        _paymentsRepository = paymentsRepository;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<PostPaymentResponse?>> GetPaymentAsync(Guid id)
+    {
+        var payment = _paymentsRepository.Get(id);
+
+        if (payment == null)
+        {
+            return NotFound();
+        }
+        return new OkObjectResult(payment);
+    }
+}
